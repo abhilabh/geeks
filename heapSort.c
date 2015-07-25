f4566deedac330a0ae8291c17b6a2c25a7e0d5fe
Abhishek Labh <kumarlabh.kumar5@gmail.com>
2015-07-25 23:00:17 +0530

    Adding heapify function

--- heapSort.c
+++ heapSort.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+//#define size 9
+int size=9;
 
 void print_arr(int arr[],int size)
 {
@@ -9,9 +11,39 @@ void print_arr(int arr[],int size)
         printf("\n");
     }
 }
+void maxHeapify(int arr[],int n)
+{
+    int l,r,large;
+  //  if(n<=1)
+   //     return;
+    l=2*n+1;
+    r=2*n+2;
+    if(l<=size && arr[n]<arr[l])
+        large=l;
+    else
+        large=n;
+    if(r<=size && arr[large]<arr[r])
+        large=r;
+    if(large!=n)
+    {
+        int temp;
+        temp=arr[n];
+        arr[n]=arr[large];
+        arr[large]=temp;
+         maxHeapify(arr,large);
+    }
+
+}
 int main()
 {
-    int arr[9]={24,26,5,74,4,14,44,18,29};
+    int arr[9]={24,26,5,74,4,14,44,18,29},i;
 
     print_arr(arr,9);
+    for(i=7/2;i>=0;i--)
+    {
+        maxHeapify(arr,i);
+
+    }
+    print_arr(arr,9);
+
 }
