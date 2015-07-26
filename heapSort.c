b0e07c349a09a79a9fbcea6adb76175f709460e6
Abhishek Labh <kumarlabh.kumar5@gmail.com>
2015-07-26 17:07:48 +0530

    complete heap sort program

--- heapSort.c
+++ heapSort.c
@@ -7,9 +7,10 @@ void print_arr(int arr[],int size)
     int i;
     for(i=0;i<size;i++)
     {
-        printf("arr[%d]=%d",i,arr[i]);
-        printf("\n");
+        printf("arr[%d]=%d ",i,arr[i]);
+       printf("\n");
     }
+         printf("\n");
 }
 void maxHeapify(int arr[],int n)
 {
@@ -18,11 +19,11 @@ void maxHeapify(int arr[],int n)
    //     return;
     l=2*n+1;
     r=2*n+2;
-    if(l<=size && arr[n]<arr[l])
+    if(l<size && arr[n]<arr[l])
         large=l;
     else
         large=n;
-    if(r<=size && arr[large]<arr[r])
+    if(r<size && arr[large]<arr[r])
         large=r;
     if(large!=n)
     {
@@ -30,20 +31,41 @@ void maxHeapify(int arr[],int n)
         temp=arr[n];
         arr[n]=arr[large];
         arr[large]=temp;
+
          maxHeapify(arr,large);
     }
 
+}
+void buildHeap(int arr[],int size)
+{
+    int i;
+     for(i=7/2;i>=0;i--)
+    {
+        maxHeapify(arr,i);
+
+    }
+}
+void heapSort(int arr[],int n)
+{
+    int i;
+    for(i=n-1;i>0;i--)
+    {
+        int temp;
+        temp=arr[0];
+        arr[0]=arr[i];
+        arr[i]=temp;
+        size--;
+              print_arr(arr,9);
+        maxHeapify(arr,0);
+    }
 }
 int main()
 {
     int arr[9]={24,26,5,74,4,14,44,18,29},i;
 
     print_arr(arr,9);
-    for(i=7/2;i>=0;i--)
-    {
-        maxHeapify(arr,i);
-
-    }
+    buildHeap(arr,9);
+    heapSort(arr,9);
     print_arr(arr,9);
 
 }
